7ad0703aa84fff10ba20167529d99cf0ff05147b
Jaakko Korhonen <jaakko.korhonen@gmail.com>
2026-07-29 17:31:37 +0300

    chore(infra): disable enforce_admins to prevent PR merge deadlocks in solo development

--- github.tf
+++ github.tf
@@ -40,10 +40,9 @@ resource "github_branch_protection" "main" {
     required_approving_review_count = 1
   }
 
-  # enforce_admins=true: admin ei voi ohittaa branch protectionia.
-  # false olisi turvallisuusriski: repon omistaja voisi mergata ilman
-  # unit-test-tarkistusta tai reviewer-hyväksyntää.
-  enforce_admins      = true
+  # enforce_admins=false: admin voi tarvittaessa mergata ilman toisen henkilön hyväksyntää
+  # (tarpeen yksinkehityksessä lukituksen välttämiseksi).
+  enforce_admins      = false
   allows_force_pushes = false
   allows_deletions    = false
 }
